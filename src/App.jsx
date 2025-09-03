87fa2fea2502d1d92188f98921258d10b9ddde94
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-09-03 19:39:11 +0530

    sringify

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 
 const App = () => {
 
+  const [msg, setMsg] = useState("Enter Your  Srtings");
+
   const [leftstr, setLeftstr] = useState("");
   const [rightstr, setRightstr] = useState("");
 
-  const [msg, setMsg] = useState("Enter Your  Srtings");
-
-  const checstring = () => {
+  const checkstring = () => {
     if (rightstr == leftstr) {
       setMsg("String Are Same");
     }
@@ -19,23 +19,22 @@ const App = () => {
   }
 
   useEffect(() => {
-    checstring();
-  }, [leftstr, rightstr])
+    checkstring();
+  }, [rightstr, leftstr])
 
   return (
-    <div>
-
+   <>
       <h1>Stringify</h1>
       <div className='container'>
 
         <div>
-          <textarea name="" id="" cols="30" rows="10" onChange={(e) => setLeftstr(e.target.value)} value={leftstr}/>
-          <textarea name="" id="" cols="30" rows="10" onChange={(e) => setRightstr(e.target.value)} value={rightstr}/>
+          <textarea name="" id="" cols="30" rows="10" onChange={(e) => setLeftstr(e.target.value)}/>
+          <textarea name="" id="" cols="30" rows="10" onChange={(e) => setRightstr(e.target.value)}/>
         </div>
-
+        
         <p>{msg}</p>
       </div>
-    </div>
+    </>
   )
 }
 
